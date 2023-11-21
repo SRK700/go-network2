f36d1fa5f659ae65ebb087d57679b9be2d56f595
SRK700 <papana.210@gmail.com>
2023-11-21 20:16:43 +0700

    -m -a

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/SRK700/go-network2/tutor2
 
 go 1.21.4
+
+require github.com/gorilla/mux v1.8.1
--- tutor2/main.go
+++ tutor2/main.go
@@ -3,19 +3,53 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        if r.URL.Path == "/" {
-            fmt.Fprint(w, "Hello, World!\n")
-        } else if r.URL.Path == "/about" {
-            fmt.Fprint(w, "My name is surasak\n")
-        } else {
-            http.NotFound(w, r)
-            return
-        }
-    })
+	r := mux.NewRouter()
 
-    http.ListenAndServe(":8080", nil)
+	r.HandleFunc("/books/{title}/page/{page}", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		title := vars["title"]
+		page := vars["page"]
+
+		fmt.Fprintf(w, "You've requested the book: %s on page %s\n", title, page)
+	})
+
+	r.HandleFunc("/cal/{num1}/plus/{num2}", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		num1Str := vars["num1"]
+		num2Str := vars["num2"]
+
+		num1, err := strconv.Atoi(num1Str)
+		if err != nil {
+			http.Error(w, "Invalid number", http.StatusBadRequest)
+			return
+		}
+
+		num2, err := strconv.Atoi(num2Str)
+		if err != nil {
+			http.Error(w, "Invalid number", http.StatusBadRequest)
+			return
+		}
+
+		total := num1 + num2
+		fmt.Fprintf(w, "Total = %d\n", total)
+	})
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "hello world")
+	})
+
+	http.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "my name is surasak")
+	})
+
+	fs := http.FileServer(http.Dir("static/"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	http.ListenAndServe(":8080", r)
 }
